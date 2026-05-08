73f89d71048b2f05980b8fb9212e525b460ddb01
Miguel Morán <miguel381@gmail.com>
2026-05-08 11:21:30 -0500

    Mi red social

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("boton_registro").addEventListener("click", function() {
+    document.body.innerHTML = '';
+    window.location.href = "redsocial.html";
+});
+
+document.getElementById("button").addEventListener("click", function() {
+    document.body.innerHTML = '';
+    window.location.href = "redsocial.html";
+});
+
